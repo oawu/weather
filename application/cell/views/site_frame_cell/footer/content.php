6edd03de3ee013e9766161b3ab11b486c90a2e0e
OA Wu <comdan66@gmail.com>
2015-07-10 06:50:30 +0800

    Fix footer text.

--- application/cell/views/site_frame_cell/footer/content.php
+++ application/cell/views/site_frame_cell/footer/content.php
@@ -2,7 +2,7 @@
   <div class='l'></div>
   <div class='c'>
     <div>Weather Maps © 2015</div>
-    <div>如有相關問題歡請聯絡管理員。</div>
+    <div>如有相關問題歡迎與<a href='https://www.facebook.com/'>作者</a>討論。</div>
   </div>
   <div class='r'></div>
 </div>
