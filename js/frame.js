e73d39882c4926ae331b9e8656280fed8f28afe4
OA Wu <comdan66@gmail.com>
2015-07-24 15:05:28 +0800

    Fix code.

--- js/frame.js
+++ js/frame.js
@@ -14,6 +14,14 @@ if (ENVIRONMENT == 'dev') {
     getWeatherByPostalCodeUrl: 'http://dev.weather.ioa.tw/api/github/get_weather_by_postal_code/',
   };
 } else {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46121102-7', 'auto');
+  ga('send', 'pageview');
+  
   window.url = 'http://comdan66.github.io/weather/';
   window.api = {
     getWeathersUrl: 'http://weather.ioa.tw/api/github/get_weathers/',
