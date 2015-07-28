d5fbc18a6bfae2a24aa87dc42700003c2d1a2a7c
OA Wu <comdan66@gmail.com>
2015-07-29 04:06:04 +0800

    Fix html code.

--- application/controllers/crontabs.php
+++ application/controllers/crontabs.php
@@ -11,31 +11,44 @@ class Crontabs extends Site_controller {
     parent::__construct ();
   }
 
-  public function clean_all_cell () {
-    clean_cell ('*');
+  public function test ($psw) {
+    if (md5 ($psw) != '6d499b8cebdc1464c46cc22a201036bd')
+      return ;
+    echo "Yes";
   }
-  public function clean_all_github_cell () {
+
+  public function clean_all_github_cell ($psw) {
     $log = CrontabLog::start ('清除 update_weather 完成後');
+
+    if (md5 ($psw) != '6d499b8cebdc1464c46cc22a201036bd')
+      return ;
+
     clean_cell ('github_cell', '*');
     $log->finish ();
   }
-  public function clean_query () {
+  public function clean_query ($psw) {
     $log = CrontabLog::start ('每 30 分鐘，清除 query logs');
+
+    if (md5 ($psw) != '6d499b8cebdc1464c46cc22a201036bd')
+      return ;
     
     $this->load->helper ('file');
     write_file (FCPATH . 'application/logs/query.log', '', FOPEN_READ_WRITE_CREATE_DESTRUCTIVE);
 
     $log->finish ();
   }
-  public function weather_all () {
+  public function weather_all ($psw) {
     $log = CrontabLog::start ('每 60 分鐘，清除 update_weather');
+
+    if (md5 ($psw) != '6d499b8cebdc1464c46cc22a201036bd')
+      return ;
 
     foreach (Town::all () as $town) {
       clean_cell ('town_cell', 'update_weather', $town->id);
       $town->update_weather ();
     }
 
-    $this->clean_all_github_cell ();
+    $this->clean_all_github_cell ($psw);
 
     $log->finish ();
   }
